3fc370edc101f04b76eba3e7c3eb0c06c1b70a03
cdroulers <droulers.christian@gmail.com>
2015-09-20 20:53:06 -0400

    Basic test layout with failing tests for a stub method.

---
+++ Cdroulers.Business.Tests.Unit/GivenPrimeFactorCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Cdroulers.Business.Tests.Unit
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class GivenPrimeFactorCalculator
+    {
+        private PrimeFactorCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new PrimeFactorCalculator();
+        }
+
+        [Test]
+        public void When_calculating_prime_factors_for_zero_Then_throws_argument_out_of_range_exception()
+        {
+            Assert.That(() => _calculator.CalculatePrimeFactors(0), Throws.Exception.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void When_calculating_prime_factors_for_one_Then_returns_one()
+        {
+            Assert.That(_calculator.CalculatePrimeFactors(1), Is.EquivalentTo(new[] { 1 }));
+        }
+    }
+}
---
+++ Cdroulers.Business/PrimeFactorCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Cdroulers.Business
+{
+    public class PrimeFactorCalculator
+    {
+        public IEnumerable<int> CalculatePrimeFactors(int number)
+        {
+            return null;
+        }
+    }
+}
